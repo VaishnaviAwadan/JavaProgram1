b3b66d120915a6bb5fa62e828c727e94f78c42f5
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2026-02-11 20:22:03 +0530

    armstrong number program

---
+++ eclipse-workspace/JavaProgram/src/Armstrong.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Armstrong {
+
+	public static void main(String[] args) {
+		int n, count = 0, a, b, c, sum = 0;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number to check Armstrong number:");
+	   n = s.nextInt();
+	   a = n;
+	   c = n;
+	   while(a > 0)
+	   {
+		   a = a/10;
+		   count++;
+	   }
+			
+       while(n > 0)
+       {
+    	   b = n % 10;
+    	   sum = (int) (sum+Math.pow(b, count));
+    	   n = n / 10;
+       }
+       if(sum == c)
+       {
+    	   System.out .println(c+ " is an armstrong number");
+    	   
+       }
+       else
+       {
+    	   System.out.println(c+  " is not an armstrong number");
+       } 
+	}
+
+}
