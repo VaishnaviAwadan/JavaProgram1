d4be9f629b7fee8740d830f4073d5af916d1d445
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2026-02-11 20:38:44 +0530

    months program added

---
+++ eclipse-workspace/JavaProgram/src/Months.java
@@ -0,0 +1,14 @@
+
+public class Months {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String[] months=new String[] {"January","February","March","April","May","June","July","August",
+    		 "September","October","November","December"};
+      for(int i=0;i<months.length;i++)
+      {
+    	  System.out.println("" + months[i]);
+     }
+	}
+
+}
