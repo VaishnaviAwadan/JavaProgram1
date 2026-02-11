51c5d1c249c1aa1c887e2927c3c129e3c2dc4510
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2026-02-12 00:15:07 +0530

    First ten prime number

---
+++ FirstTenPrime.java
@@ -0,0 +1,32 @@
+
+public class FirstTenPrime {
+
+	public static void main(String[] args) {
+		        int count = 0;
+		        int number = 2;
+		        
+		        System.out.println("First 10 prime numbers:");
+		        
+		        while (count < 10) {
+		            if (isPrime(number)) {
+		                System.out.print(number + " ");
+		                count++;
+		            }
+		            number++;
+		        }
+		        }
+		    public static boolean isPrime(int num) {
+		        if (num <= 1) {
+		            return false;
+		        }
+		        for (int i = 2; i <= Math.sqrt(num); i++) {
+		            if (num % i == 0) {
+		                return false;
+		            }
+		        }
+		        
+		        return true;
+		    }
+
+	}
+
