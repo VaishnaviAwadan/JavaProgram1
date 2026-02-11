a7493ee4b3b93b9e74fa78672e4d7c9318623b51
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2026-02-11 23:23:08 +0530

    program for convert Dollar into Rupees

---
+++ eclipse-workspace/JavaProgram/src/DollarIntoRupees.java
@@ -0,0 +1,11 @@
+
+public class DollarIntoRupees {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int Dollar=500;
+     System.out.println("Dollar=" +Dollar);
+   double  Rupees=Dollar * 83.27;
+     System.out.println("500 Dollar into Rupees= "+Rupees);
+	}
+}
